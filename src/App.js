57a26d032b503aa1e0a90faf323ccf422f01bea8
Adhi <adhipathipandiyan.s12@gmail.com>
2022-03-03 13:37:43 +0530

    Uploading Files to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import QrCode from './images/image-qr-code.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<section className="App">
+			<div className="card">
+				<div className="card-image-holder">
+					<div className="card-image">
+						<img src={QrCode} alt="QR Code Image" />
+					</div>
+				</div>
+				<div className="card-desc-holder">
+					<div className="card-title">Imrpove your front-end skills by building projects</div>
+					<div className="card-desc">
+						Scan the QR Code to visit Front-end mentor and take coding skills to next level
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
